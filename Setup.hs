8c20938a013b68f9077937cdf12aaac989fb2bba
Rogério Chaves <rogeriocfj@gmail.com>
2015-12-21 23:22:20 -0200

    Download csv directly

--- Setup.hs
+++ Setup.hs
@@ -1,16 +1,23 @@
-import Data.Quandl
-import Data.Time (fromGregorian)
-import System.Environment
+import System.Environment (getEnv)
+import Control.Applicative ((<$>))
+import Data.ByteString.Char8 (pack, unpack)
+import Network.HTTP.Conduit
+import Data.String.Utils (replace)
 
 main = do
+  stock <- return "WIKI/FB"
   token <- getEnv "QUANDL_KEY"
-  dataset <- getTableWith (defaultOptions {
-                              opSortAscending  = True,
-                              opAuthToken      = Just token,
-                              opStartDate      = Just (fromGregorian 2015 12 1),
-                              opEndDate        = Just (fromGregorian 2015 12 21),
-                              opFrequency      = Just Daily,
-                              opTransformation = Nothing})
-             [("WIKI", "AAPL", Nothing)]
-  print $ fmap daData dataset
+  request <- getStock stock token
+  manager <- newManager tlsManagerSettings
+  response <- httpLbs request manager
+  return $ responseBody response
+  writeFile ("data/" ++ (replace "/" "-" stock) ++ ".csv") $ show $ responseBody response
+  print "Done"
 
+getStock stock token = setQueryString query <$> url
+         where
+          url = parseUrl $ "https://www.quandl.com/api/v3/datasets/" ++ stock ++ "/data.csv"
+          query = queryParam <$> [("api_key", token),
+                                  ("transform", "rdiff")]
+
+queryParam (a, b) = (pack a, (Just (pack b)))
