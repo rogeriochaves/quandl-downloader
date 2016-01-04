bf0eb458e72ae23417000f2dfc6f674685cd5458
Rogério Chaves <rogeriocfj@gmail.com>
2016-01-04 10:19:54 -0200

    Refactoring

--- Main.hs
+++ Main.hs
@@ -3,31 +3,45 @@ import Control.Applicative ((<$>))
 import Data.ByteString.Char8 (pack, unpack)
 import Network.HTTP.Conduit
 import Data.String.Utils (replace)
+import Data.ByteString.Internal (ByteString)
+import Control.Monad.Catch (MonadThrow)
+
+type Stock = String
+type Token = String
 
 main = do
   args <- getArgs
-  mapM downloadStockCsv args
+  manager <- newManager tlsManagerSettings
+  mapM (downloadStockCsv manager) args
   putStrLn "Done"
 
-downloadStockCsv stock = do
+downloadStockCsv :: Manager -> Stock -> IO ()
+downloadStockCsv manager stock = do
   putStrLn $ "Downloading " ++ stock
-  manager <- newManager tlsManagerSettings
-  response <- getStock stock manager
-  writeFile ("data/" ++ (replace "/" "-" stock) ++ ".csv") $ show $ responseBody response
+  response <- getStock manager stock
+  writeFile (filePathFor stock) response
   putStrLn "Finished"
 
-getStock stock manager = do
+getStock :: Manager -> Stock -> IO String
+getStock manager stock = do
   request <- getStockRequestWithEnvToken stock
-  httpLbs request manager
+  response <- httpLbs request manager
+  return $ show (responseBody response)
 
+getStockRequestWithEnvToken :: Stock -> IO Request
 getStockRequestWithEnvToken stock = do
   token <- getEnv "QUANDL_KEY"
   getStockRequest stock token
 
+getStockRequest :: (MonadThrow f, Functor f) => Stock -> Token -> f Request
 getStockRequest stock token = setQueryString query <$> url
          where
           url = parseUrl $ "https://www.quandl.com/api/v3/datasets/" ++ stock ++ "/data.csv"
           query = queryParam <$> [("api_key", token),
                                   ("transform", "rdiff")]
 
-queryParam (a, b) = (pack a, (Just (pack b)))
+queryParam :: (String, String) -> (ByteString, Maybe ByteString)
+queryParam (a, b) = (pack a, Just (pack b))
+
+filePathFor :: Stock -> String
+filePathFor stock = "data/" ++ (replace "/" "-" stock) ++ ".csv"
